71714ec7bd7493075756b890d117e2a0e2a6bcfd
Jakub Ruttkay <jakubruttkay@gmail.com>
2021-01-07 17:32:50 +1300

    fix

--- calc/math.go
+++ calc/math.go
@@ -6,7 +6,7 @@ import "errors"
 func Add(numbers ...int) (int, error) {
 	var sum int
 	if len(numbers) < 2 {
-		return nil, errors.New("Provide at least 2 numbers"),
+		return 0, errors.New("Provide at least 2 numbers")
 	} else {
 		for _, i := range numbers {
 			sum = sum + i
